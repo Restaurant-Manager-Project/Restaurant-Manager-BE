0780f7672e92f0a93db6d03a0523c6541fd43594
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-10-09 08:59:39 +0700

    init project

---
+++ src/main/java/com/example/Restaurant_Manager_BE/RestaurantManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Manager_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Restaurant_Manager_BE/RestaurantManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Restaurant_Manager_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
