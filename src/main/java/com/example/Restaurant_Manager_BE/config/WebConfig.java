7c88a328af0c522504a7ec83f14ada5c9fb9c049
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-10-14 10:16:43 +0700

    config cors

---
+++ src/main/java/com/example/Restaurant_Manager_BE/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.Restaurant_Manager_BE.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Áp dụng cho tất cả các endpoint
+                .allowedOrigins("*") // Cho phép tất cả các origin (có thể thay bằng danh sách cụ thể)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức HTTP được phép
+                .allowedHeaders("*") // Các headers được phép
+                .allowCredentials(true); // Cho phép cookie và thông tin xác thực
+    }
+}
