5cc6d337861b0b623d224de44821d7288937ff56
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-10-24 15:10:14 +0700

    feat: update find, create clients

--- src/main/java/com/example/Restaurant_Manager_BE/services/OrderService.java
+++ src/main/java/com/example/Restaurant_Manager_BE/services/OrderService.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public interface OrderService {
     ResponseEntity<APIResponse> createOrder(OrderDTO orderDTO);
-
     ResponseEntity<APIResponse> getOrdersByDirection(String direction);
 
 }
