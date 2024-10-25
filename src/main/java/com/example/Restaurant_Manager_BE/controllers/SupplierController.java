1a5bd0a77e25014e25e9756e3495742b89527f64
Thong <huynhthong12300@gmail.com>
2024-10-25 19:30:50 +0700

    SupplierController

--- src/main/java/com/example/Restaurant_Manager_BE/controllers/SupplierController.java
+++ src/main/java/com/example/Restaurant_Manager_BE/controllers/SupplierController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +32,9 @@ public class SupplierController {
         return supplierService.getByName(params.get("name"));
     }
 
+    @Operation(summary = "Tìm kiếm nhà cung cấp theo id")
+    @GetMapping("/api/supplier/{id}")
+    public ResponseEntity<APIResponse> getSupplierById(@PathVariable("id") Long id) {
+        return supplierService.getById(id);
+    }
 }
