f966d551793e7a99f70d07980ccb9365215cefdb
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-11-09 19:40:15 +0700

    update import p3

--- src/main/java/com/example/Restaurant_Manager_BE/dto/ImportDTO.java
+++ src/main/java/com/example/Restaurant_Manager_BE/dto/ImportDTO.java
@@ -3,22 +3,26 @@ package com.example.Restaurant_Manager_BE.dto;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ImportDTO {
     private Long id;
     private Long employeeId;
     private String employeeName;
     private Long supplierId;
     private String supplierName;
-    private Date dateCreate;
+    private String dateCreate;
     private Long total;
 
     @JsonProperty("detailsProductList")
---
+++ src/main/java/com/example/Restaurant_Manager_BE/utils/FormatUtil.java
@@ -0,0 +1,7 @@
+package com.example.Restaurant_Manager_BE.utils;
+
+import java.text.SimpleDateFormat;
+
+public class FormatUtil {
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+}
