ab7586bf47fb4e571d925303fda1bab0879ae96b
Ginwnn04 <quangdeeptry1911@gmail.com>
2024-11-13 14:47:48 +0700

    update security 4

--- src/main/java/com/example/Restaurant_Manager_BE/configurations/WebConfig.java
+++ src/main/java/com/example/Restaurant_Manager_BE/configurations/WebConfig.java
@@ -8,12 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("*") // Cho phép tất cả các origin (có thể thay bằng danh sách cụ thể)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Các phương thức HTTP được phép
-
-
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // Áp dụng cho tất cả các endpoint
+//                .allowedOrigins("*") // Cho phép tất cả các origin (có thể thay bằng danh sách cụ thể)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Các phương thức HTTP được phép
+//
+//
+//    }
 }
--- src/main/java/com/example/Restaurant_Manager_BE/controllers/AccountController.java
+++ src/main/java/com/example/Restaurant_Manager_BE/controllers/AccountController.java
@@ -5,6 +5,9 @@ import com.example.Restaurant_Manager_BE.responses.APIResponse;
 import com.example.Restaurant_Manager_BE.services.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,15 @@ public class AccountController {
     @PostMapping("/accounts")
     public ResponseEntity<APIResponse> createAccount(@RequestBody AccountDTO accountDTO) {
         return accountService.createAccount(accountDTO);
+    }
+
+    @GetMapping("/accounts/test")
+    public ResponseEntity<APIResponse> test() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("username: " + authentication.getName());
+        authentication.getAuthorities().forEach(authority -> {
+            System.out.println("authority: " + authority.getAuthority());
+        });
+        return null;
     }
 }
